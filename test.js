fdeaf0a82d9fd740b71bc7f01323a4773a2c4868
мансур зурабов <asl.zurabov@gmail.com>
2022-09-01 22:34:18 +0300

    ddd

--- test.js
+++ test.js
@@ -7,7 +7,6 @@ function factorial(n)
   return fc
 }
 
-console.log(factorial(5))
 
 
 function reverseWords(str) {
@@ -36,7 +35,6 @@ function reverseWords(str) {
 
   function findOdd(A) {
     dic = {}
-    
     for (i=0; i<A.length;i++){
       if (A[i] in dic){
         dic[A[i]] += 1
@@ -44,15 +42,44 @@ function reverseWords(str) {
         dic[A[i]] = 1
       }
     }
-   
+    c = ''
+    for (key in dic){
+      if (dic[key] % 2 != 0){
+        return parseInt(String(key))
+      }
+    }
   }
 
-  console.log(findOdd([1,2,3,4,5,4,3,2,2,2,2]))
+  function moveZeros(arr) {
+    count = 0
+    for (i in arr){
+        console.log(i)
+      if (arr[i] == 0){
+        count++
+       arr.slice
+      }
+      
+    }
+    console.log(arr)
+    for (i=0; i < count; i++){
+      lst.push(0)
+    }
+    
+
+}
+
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
+
+console.log('this is the test',false == 0)
 
 
-  d = {'1':3,'2':43}
-  c = ''
-  for (e in d){
-    c += e
-  }
-  console.log(c)
+
+
+function flt(integer_list,values_list){
+    for (i=0; i < values_list.length; i++){
+        integer_list.filter(data => data != values_list[i])
+      }
+      return integer_list
+}
+
+console.log('-----------------',flt([1, 1, 2 ,3 ,1 ,2 ,3 ,4],[1, 3]))
